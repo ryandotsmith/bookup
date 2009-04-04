b2a9e0442f0f60f28709a976c4cf32b0c1e99bf7
Ryan Smith <this.ryansmith@gmail.com>
2009-04-04 09:41:47 -0500

    removed zebra function from the tablesorter on the books show page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,5 +2,5 @@ $(document).ready( function() {
 	$("#session_bar").corners('10px');
 	$("#waiting_divs").corners('10px');
 	$("#books_index_table").tablesorter({ widgets: ['zebra']});
-	$("#show_book_listings_table").tablesorter({ widgets: ['zebra']});
+	$("#show_book_listings_table").tablesorter();
 })//document
