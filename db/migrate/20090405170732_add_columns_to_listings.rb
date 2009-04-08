85979ad6221d87b83cb298f1e9cf3e9f0616dabd
Ryan Smith <this.ryansmith@gmail.com>
2009-04-08 17:39:32 -0500

    added interesting trades to listings

--- db/migrate/20090405170732_add_columns_to_listings.rb
+++ db/migrate/20090405170732_add_columns_to_listings.rb
@@ -1,6 +1,8 @@
 class AddColumnsToListings < ActiveRecord::Migration
   def self.up
     add_column :listings, :price, :float
+    add_column :listings, :interesting_trades, :boolean
+
   end
 
   def self.down
