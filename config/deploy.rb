fe83e4d371d4fe2201e9b9ab092c7384d47bc0f6
Ryan Smith <this.ryansmith@gmail.com>
2009-04-09 13:23:24 -0500

    added some gems to the environment

---
+++ config/deploy.rb
@@ -0,0 +1,56 @@
+#############################################################
+#	Application
+#############################################################
+
+set :application, "bookup"
+set :deploy_to, "/home/rsmith/app/#{application}"
+
+#############################################################
+#	Settings
+#############################################################
+
+default_run_options[:pty] = true
+set :use_sudo, false
+
+#############################################################
+#	Servers
+#############################################################
+
+set :user, "rsmith"
+set :domain, "174.143.27.236"
+server domain, :app, :web
+role :db, domain, :primary => true
+
+#############################################################
+#	Git
+#############################################################
+set :scm, :git
+set :repository, "git@github.com:ryandotsmith/bookup.git"
+set :branch, "master"
+set :deploy_via, :remote_cache
+
+
+#############################################################
+#	Passenger
+#############################################################
+namespace :deploy do
+  
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+    run "mkdir #{current_path}/tmp/sessions"
+  end
+
+  desc "Symlink shared configs and folders on each release."
+    task :symlink_shared do
+      run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+      run "git submodule init"
+      run "git submodule update"
+      
+    end
+      
+end
+after 'deploy:update_code', 'deploy:symlink_shared'
+#############################################################
+#	Attachment_FU 
+#############################################################
